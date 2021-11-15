04ad9cc4d805f60863b8d5c099aa4658a9c40f69
Jordy Morales <jordy.morales@nearshorecode.com>
2021-11-15 12:56:56 -0400

    Installing Material UI and configuring themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from '@mui/material/Button';
+import { createCustomTheme } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import useSettings from './hooks/useSettings';
 
-function App() {
+const App: React.FC = () => {
+  const { settings } = useSettings();
+
+  const theme = createCustomTheme({
+    direction: settings.direction,
+    responsiveFontSizes: settings.responsiveFontSizes,
+    roundedCorners: settings.roundedCorners,
+    theme: settings.theme,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Button variant="contained">Hello World</Button>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
